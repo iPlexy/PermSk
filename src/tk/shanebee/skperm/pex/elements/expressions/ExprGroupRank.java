999cd6b452e2d3328cc28d0d946454b32b422851
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-02-08 14:01:09 -0800

    ExprGroupRank - Redo

--- src/tk/shanebee/skperm/pex/elements/expressions/ExprGroupRank.java
+++ src/tk/shanebee/skperm/pex/elements/expressions/ExprGroupRank.java
@@ -17,7 +17,6 @@ import tk.shanebee.skperm.utils.api.API;
 @Description("Set the rank of a group, also supports add, remove, reset and get. Currently only supports PEX")
 @Examples({"set rank of group \"mod\" to 100", "reset rank of group \"admin\"",
         "set {_rank} to rank of group \"owner\""})
-@RequiredPlugins({"Vault", "PermissionsEX"})
 @Since("2.0.0")
 public class ExprGroupRank extends SimpleExpression<Number> {
 
