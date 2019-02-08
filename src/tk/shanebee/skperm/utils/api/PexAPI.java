d28593b9561794e105cc8667879c22fedb68e1ac
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-02-08 13:31:26 -0800

    New PEX API

---
+++ src/tk/shanebee/skperm/utils/api/PexAPI.java
@@ -0,0 +1,122 @@
+package tk.shanebee.skperm.utils.api;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.World;
+import ru.tehkode.permissions.PermissionGroup;
+import ru.tehkode.permissions.PermissionUser;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PexAPI implements API {
+
+    public void addPerm(OfflinePlayer player, String permission) {
+        PermissionsEx.getUser(player.getName()).addPermission(permission);
+    }
+
+    public void addPerm(OfflinePlayer player, String permission, World world) {
+        PermissionsEx.getUser(player.getName()).addPermission(permission, world.getName());
+    }
+
+    public void addPerm(OfflinePlayer player, String permission, World world, int seconds) {
+        PermissionsEx.getUser(player.getName()).addTimedPermission(permission, world.getName(), seconds);
+    }
+
+    public void addPerm(String group, String permission) {
+        PermissionsEx.getPermissionManager().getGroup(group).addPermission(permission);
+    }
+
+    public void addPerm(String group, String permission, World world) {
+        PermissionsEx.getPermissionManager().getGroup(group).addPermission(permission, world.getName());
+    }
+
+    public void addPerm(String group, String permission, World world, int seconds) {
+        PermissionsEx.getPermissionManager().getGroup(group).addTimedPermission(permission, world.getName(), seconds);
+    }
+
+    public void removePerm(OfflinePlayer player, String permission) {
+        PermissionsEx.getUser(player.getName()).removePermission(permission);
+    }
+
+    public void removePerm(OfflinePlayer player, String permission, World world) {
+        PermissionsEx.getUser(player.getName()).removePermission(permission, world.getName());
+        
+    }
+
+    public void removePerm(String group, String permission) {
+        PermissionsEx.getPermissionManager().getGroup(group).removePermission(permission);
+    }
+
+    public void removePerm(String group, String permission, World world) {
+        PermissionsEx.getPermissionManager().getGroup(group).removePermission(permission, world.getName());
+    }
+
+    public String[] getPerm(OfflinePlayer player) {
+        PermissionUser user = PermissionsEx.getUser(player.getName());
+        return user.getPermissions(null).toArray(new String[0]);
+    }
+
+    public String[] getPerm(OfflinePlayer player, World world) {
+        PermissionUser user = PermissionsEx.getUser(player.getName());
+        return user.getPermissions(world.getName()).toArray(new String[0]);
+    }
+
+    public String[] getPerm(String group) {
+        PermissionGroup groupT = PermissionsEx.getPermissionManager().getGroup(group);
+        return groupT.getPermissions(null).toArray(new String[0]);
+    }
+
+    public String[] getPerm(String group, World world) {
+        PermissionGroup groupT = PermissionsEx.getPermissionManager().getGroup(group);
+        return groupT.getPermissions(world.getName()).toArray(new String[0]);
+    }
+
+    public void createGroup(String group) {
+        PermissionGroup pexGroup = PermissionsEx.getPermissionManager().getGroup(group);
+        pexGroup.save();
+    }
+
+    public void createGroup(String group, String[] parents) {
+        PermissionGroup pexGroup = PermissionsEx.getPermissionManager().getGroup(group);
+        List<PermissionGroup> groups = new LinkedList<>();
+        for (String parent : parents) {
+            groups.add(PermissionsEx.getPermissionManager().getGroup(parent));
+        }
+        pexGroup.setParents(groups, null);
+        pexGroup.save();
+    }
+
+    public void removeGroup(String group) {
+        PermissionsEx.getPermissionManager().getGroup(group).remove();
+    }
+
+    public void addPlayerToGroup(OfflinePlayer player, String group) {
+        PermissionsEx.getUser(player.getName()).addGroup(group);
+    }
+
+    public void addPlayerToGroup(OfflinePlayer player, String group, World world) {
+        PermissionsEx.getUser(player.getName()).addGroup(group, world.getName());
+    }
+
+    public void addPlayerToGroup(OfflinePlayer player, String group, World world, int seconds) {
+        PermissionsEx.getUser(player.getName()).addGroup(group, world.getName(), seconds);
+    }
+
+    public void removePlayerFromGroup(OfflinePlayer player, String group) {
+        PermissionsEx.getUser(player.getName()).removeGroup(group);
+    }
+
+    public void removePlayerFromGroup(OfflinePlayer player, String group, World world) {
+        PermissionsEx.getUser(player.getName()).removeGroup(group, world.getName());
+    }
+
+    public void setGroupWeight(String group, int weight) {
+        PermissionsEx.getPermissionManager().getGroup(group).setWeight(weight);
+    }
+
+    public void setGroupRank(String group, int rank) {
+        PermissionsEx.getPermissionManager().getGroup(group).setRank(rank);
+    }
+
+}
