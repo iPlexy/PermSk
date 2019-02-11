62ffe8b2b03c283a0880d5a89639baf412da75c4
ShaneBeee shanebolenback@me.com <shanebolenback@me.com>
2019-02-10 23:05:50 -0800

    Cleaning things up

--- src/tk/shanebee/skperm/SkPerm.java
+++ src/tk/shanebee/skperm/SkPerm.java
@@ -8,73 +8,53 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import tk.shanebee.skperm.utils.api.API;
-import tk.shanebee.skperm.utils.api.LuckAPI;
-import tk.shanebee.skperm.utils.api.PexAPI;
-import tk.shanebee.skperm.utils.api.UltraAPI;
 
 public class SkPerm extends JavaPlugin {
 
-    public static Permission perms = null;
+    private static Permission perms;
     private static API api;
+    private SkriptAddon addon;
     private String prefix = ChatColor.translateAlternateColorCodes('&', "&7[&bSk-Perm&7] ");
 
     @Override
     public void onEnable() {
-
         if ((Bukkit.getPluginManager().getPlugin("Skript") != null) && (Skript.isAcceptRegistrations())) {
-            SkriptAddon addon = Skript.registerAddon(this);
+            addon = Skript.registerAddon(this);
             try {
                 addon.loadClasses("tk.shanebee.skperm.vault", "elements");
-                sendConsoleMessage(prefix + ChatColor.GREEN + "[Skript] Dependency found");
+                sendConsoleMessage("&7[&bSkript&7]&a Dependency found, loading Skript syntaxes");
             } catch (Exception e) {
-                sendConsoleMessage(prefix + ChatColor.RED + "Loading error, try restarting your server");
+                sendConsoleMessage("&cLoading error, try restarting your server");
                 Bukkit.getPluginManager().disablePlugin(this);
                 return;
             }
             if (Bukkit.getPluginManager().getPlugin("Vault") != null) {
                 setupPermissions();
-                sendConsoleMessage(prefix + ChatColor.GREEN + "[Vault] Dependency found, Vault syntaxes loaded");
+                sendConsoleMessage("&7[&bVault&7] &aDependency found, Vault syntaxes loaded");
             } else {
-                sendConsoleMessage(prefix + ChatColor.RED + "[Vault] Dependency not found, plugin disabling");
+                sendConsoleMessage("&7[&eVault&7]&c Dependency not found, plugin disabling");
                 Bukkit.getPluginManager().disablePlugin(this);
+                return;
             }
             if (Bukkit.getPluginManager().getPlugin("PermissionsEX") != null) {
-                try {
-                    addon.loadClasses("tk.shanebee.skperm.permPlugins", "elements");
-                    api = (API) Class.forName(PexAPI.class.getName()).newInstance();
-                    permPluginFound("PEX");
-                } catch (Exception e) {
-                    permPluginLoadingError("PEX");
-                }
+                loadApi("PermissionsEX", "PexAPI");
             } else if (Bukkit.getPluginManager().getPlugin("LuckPerms") != null) {
-                try {
-                    addon.loadClasses("tk.shanebee.skperm.permPlugins", "elements");
-                    api = (API) Class.forName(LuckAPI.class.getName()).newInstance();
-                    permPluginFound("LuckPerms");
-                } catch (Exception e) {
-                    permPluginLoadingError("LuckPerms");
-                }
+                loadApi("LuckPerms", "LuckAPI");
             } else if (Bukkit.getPluginManager().getPlugin("UltraPermissions") != null) {
-                try {
-                    addon.loadClasses("tk.shanebee.skperm.permPlugins", "elements");
-                    api = (API) Class.forName(UltraAPI.class.getName()).newInstance();
-                    permPluginFound("UltraPermissions");
-                } catch (Exception e) {
-                    permPluginLoadingError("UltraPermissions");
-                }
+                loadApi("UltraPermissions", "UltraAPI");
             } else {
-                sendConsoleMessage(prefix + ChatColor.YELLOW + "[PermPlugin] Dependency not found, ignoring PermPlugin syntaxes");
+                sendConsoleMessage("&7[&ePermPlugin&7]&e No permission plugin found, ignoring PermPlugin syntaxes");
             }
-            sendConsoleMessage(prefix + ChatColor.GREEN + "Loaded successfully");
+            sendConsoleMessage("&aAdd-on loaded successfully");
         } else {
-            getLogger().info(ChatColor.RED + "[Skript] Dependency was not found, plugin disabling");
+            sendConsoleMessage("&c[Skript] Dependency was not found, plugin disabling");
             Bukkit.getPluginManager().disablePlugin(this);
         }
     }
 
     @Override
     public void onDisable() {
-        sendConsoleMessage(prefix + "Unloaded successfully");
+        sendConsoleMessage("Unloaded successfully");
     }
 
     private boolean setupPermissions() {
@@ -84,21 +64,35 @@ public class SkPerm extends JavaPlugin {
     }
 
     private void sendConsoleMessage(String message) {
-        Bukkit.getConsoleSender().sendMessage(message);
+        Bukkit.getConsoleSender().sendMessage(prefix + ChatColor.translateAlternateColorCodes('&', message));
     }
 
     private void permPluginFound(String plugin) {
         String plug = ChatColor.translateAlternateColorCodes('&', "&7[&b" + plugin + "&7] ");
-        Bukkit.getConsoleSender().sendMessage(prefix + plug + ChatColor.GREEN + "Dependency found, permission plugin syntaxes loaded");
+        sendConsoleMessage(plug + "&aDependency found, permission plugin syntaxes loaded");
     }
 
     private void permPluginLoadingError(String plugin) {
         String plug = ChatColor.translateAlternateColorCodes('&', "&7[&b" + plugin + "&7] ");
-        Bukkit.getConsoleSender().sendMessage(prefix + plug + ChatColor.RED + "Loading error, try restarting your server");
+        sendConsoleMessage(plug + "&aLoading error, try restarting your server");
+    }
+
+    private void loadApi(String permPlugin, String API) {
+        try {
+            addon.loadClasses("tk.shanebee.skperm.permPlugins", "elements");
+            api = (API) Class.forName("tk.shanebee.skperm.utils.api." + API).newInstance();
+            permPluginFound(permPlugin);
+        } catch (Exception e) {
+            permPluginLoadingError(permPlugin);
+        }
     }
 
     public static API getAPI() {
         return api;
+    }
+
+    public static Permission getPerms(){
+        return perms;
     }
 
 }
